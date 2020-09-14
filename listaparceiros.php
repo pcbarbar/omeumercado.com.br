dd18ab1b8acd32cbe5578211a41d9489ce744c6f
pcbarbar <pcbarbar@hotmail.com>
2020-09-14 19:51:44 -0300

    configurando menu parceiro

--- listaparceiros.php
+++ listaparceiros.php
@@ -35,7 +35,7 @@
       </div>
 
       <div class="container border margem-superior-30">
-        <a href="parceiro/index.php"><img src="imagens/templistaparceiros.jpg" class="img-fluid align-middle" alt="Imagem responsiva"></a>
+        <a href="parceiro/index.html"><img src="imagens/templistaparceiros.jpg" class="img-fluid align-middle" alt="Imagem responsiva"></a>
       </div>
 <!-- RETORNO DA BUSCA - INÍCIO -->
 
---
+++ starter.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    
+    <?php include 'templates/header.html'; ?>
+
+    <style>
+    </style>
+
+</head>
+<body>
+
+
+
+    <?php include 'templates/menu.html'; ?>
+
+
+    <!-- CORPO -->
+
+
+    <?php include 'templates/footer.html'; ?>
+
+</body>
+</html>
